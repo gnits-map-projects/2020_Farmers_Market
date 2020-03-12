0460c556d9cc7501c8d75612ff13b36918d47014
shashikantudapi <shashikantudapi@Shashikants-MacBook-Pro.local>
2020-03-12 17:53:43 +0530

    Added Scheduler for repetitive tasks

---
+++ service/app/utils/Scheduler.java
@@ -0,0 +1,36 @@
+package utils;
+
+import akka.actor.ActorSystem;
+import scala.concurrent.ExecutionContext;
+import scala.concurrent.duration.Duration;
+
+import javax.inject.Inject;
+import java.util.concurrent.TimeUnit;
+
+public class Scheduler {
+
+    private final ActorSystem actorSystem;
+    private final ExecutionContext executionContext;
+
+    @Inject
+    public Scheduler(ActorSystem actorSystem, ExecutionContext executionContext) {
+        this.actorSystem = actorSystem;
+        this.executionContext = executionContext;
+
+        this.initialize();
+    }
+
+    private void initialize() {
+        this.actorSystem
+                .scheduler()
+                .schedule(
+                        Duration.create(10, TimeUnit.SECONDS), // initialDelay
+                        Duration.create(1, TimeUnit.MINUTES), // interval
+                        () -> repetitiveTask(),
+                        this.executionContext);
+    }
+
+    private void repetitiveTask() {
+        System.out.println("############ TEST SCHEDULER ############");
+    }
+}
